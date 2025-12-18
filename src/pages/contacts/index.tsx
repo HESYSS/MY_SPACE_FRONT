c835ffb29f080664a0a4533940fa65d16e547d0e
Seva <masterseva2016@gmail.com>
2025-12-18 04:57:27 +0200

    Add Telegram and Viber icons; update imports in ContactsPage

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -2,12 +2,12 @@ import React, { memo } from "react";
 import Image from "next/image";
 import styles from "./contacts.module.css";
 import { useTranslation } from "react-i18next";
-import { useModal } from '../../hooks/useModal'; 
+import { useModal } from "../../hooks/useModal";
 import Link from "next/link";
 
 import youtubeIcon from "../../../public/icons/youtube.svg";
-import telegramIcon from "../../../public/icons/telega.png";
-import viberIcon from "../../../public/icons/123.jpg";
+import telegramIcon from "../../../public/icons/telegram-icon.png";
+import viberIcon from "../../../public/icons/viber-icon.png";
 import instagramIcon from "../../../public/icons/instagram.svg";
 import arrowRight from "../../../public/icons/line.svg";
 import star from "../../../public/icons/star.svg";
@@ -22,7 +22,7 @@ const ContactsPage: React.FC = () => {
 
       <div className={styles.titleArea}>
         <div className={styles.titleLine}></div>
-        <h2 className={styles.readMoreTitle}>{t('contactsTitle')}</h2>
+        <h2 className={styles.readMoreTitle}>{t("contactsTitle")}</h2>
       </div>
 
       <div className={styles.contactsBlockWrapper}>
@@ -33,12 +33,14 @@ const ContactsPage: React.FC = () => {
                 <div className={styles.contactIconContainer}>
                   <Image
                     src={star}
-                    alt={t('starAlt')}
+                    alt={t("starAlt")}
                     className={styles.starIcon}
                   />
                 </div>
                 <div className={styles.contactDetails}>
-                  <h4 className={styles.contactTitle}>{t('socialNetworksTitle')}</h4>
+                  <h4 className={styles.contactTitle}>
+                    {t("socialNetworksTitle")}
+                  </h4>
                   <div className={styles.socialLinks}>
                     <a
                       href="https://www.youtube.com/@MySpace-kyiv"
@@ -68,7 +70,7 @@ const ContactsPage: React.FC = () => {
                       />
                       <p className={styles.socialText}>Instagram</p>
                     </a>
-                      <a
+                    <a
                       href="https://t.me/+380687777337"
                       target="_blank"
                       rel="noopener noreferrer"
@@ -78,7 +80,7 @@ const ContactsPage: React.FC = () => {
                         src={telegramIcon}
                         alt="Telegram"
                         width={22}
-                        height={16}
+                        height={22}
                       />
                       <p className={styles.socialText}>Telegram</p>
                     </a>
@@ -92,7 +94,7 @@ const ContactsPage: React.FC = () => {
                         src={viberIcon}
                         alt="Viber"
                         width={22}
-                        height={16}
+                        height={22}
                       />
                       <p className={styles.socialText}>Viber</p>
                     </a>
@@ -104,12 +106,12 @@ const ContactsPage: React.FC = () => {
                 <div className={styles.contactIconContainer}>
                   <Image
                     src={star}
-                    alt={t('starAlt')}
+                    alt={t("starAlt")}
                     className={styles.starIcon}
                   />
                 </div>
                 <div className={styles.contactDetails}>
-                  <h4 className={styles.contactTitle}>{t('emailTitle')}</h4>
+                  <h4 className={styles.contactTitle}>{t("emailTitle")}</h4>
                   <p className={styles.contactInfo}>office@myspace.in.ua</p>
                 </div>
               </div>
@@ -118,13 +120,17 @@ const ContactsPage: React.FC = () => {
                 <div className={styles.contactIconContainer}>
                   <Image
                     src={star}
-                    alt={t('starAlt')}
+                    alt={t("starAlt")}
                     className={styles.starIcon}
                   />
                 </div>
                 <div className={styles.contactDetails}>
-                  <h4 className={styles.contactTitle}>{t('phoneNumberTitle')}</h4>
-                  <a href="tel:+380687777337" className={styles.contactInfo}>+38 068 777 73 37</a>
+                  <h4 className={styles.contactTitle}>
+                    {t("phoneNumberTitle")}
+                  </h4>
+                  <a href="tel:+380687777337" className={styles.contactInfo}>
+                    +38 068 777 73 37
+                  </a>
                 </div>
               </div>
 
@@ -132,28 +138,26 @@ const ContactsPage: React.FC = () => {
                 <div className={styles.contactIconContainer}>
                   <Image
                     src={star}
-                    alt={t('starAlt')}
+                    alt={t("starAlt")}
                     className={styles.starIcon}
                   />
                 </div>
                 <div className={styles.contactDetails}>
-                  <h4 className={styles.contactTitle}>{t('addressTitle')}</h4>
-                  <p className={styles.contactInfo}>
-                    {t('addressText')}
-                  </p>
+                  <h4 className={styles.contactTitle}>{t("addressTitle")}</h4>
+                  <p className={styles.contactInfo}>{t("addressText")}</p>
                 </div>
               </div>
             </div>
 
             <div className={styles.bottomRow}>
               <div className={styles.workHours}>
-                <h4 className={styles.contactTitle}>{t('workHoursTitle')}</h4>
+                <h4 className={styles.contactTitle}>{t("workHoursTitle")}</h4>
                 <div className={styles.line}></div>
                 <div className={styles.hoursDetails}>
-                  <p className={styles.workDay}>{t('workDays')}</p>
-                  <p className={styles.workDay}>{t('weekend')}</p>
+                  <p className={styles.workDay}>{t("workDays")}</p>
+                  <p className={styles.workDay}>{t("weekend")}</p>
                   <p className={styles.workTime}>09:00-19:00</p>
-                  <p className={styles.workTime}>{t('byAppointment')}</p>
+                  <p className={styles.workTime}>{t("byAppointment")}</p>
                 </div>
               </div>
             </div>
@@ -162,20 +166,22 @@ const ContactsPage: React.FC = () => {
       </div>
 
       <div className={styles.coworking}>
-        <h3 className={styles.callToAction}>{t('consultationCallToAction')}</h3>
+        <h3 className={styles.callToAction}>{t("consultationCallToAction")}</h3>
         <div className={styles.frame89}>
           <div className={styles.frame88}>
             <div
               className={styles.optionRow}
-              onClick={() => openModal('forSellers')}
+              onClick={() => openModal("forSellers")}
             >
               <div className={styles.frame87}>
                 <div className={styles.line16}></div>
                 <div className={styles.frame86}>
-                  <p className={styles.optionText}>{t('forSellersLandlords')}</p>
+                  <p className={styles.optionText}>
+                    {t("forSellersLandlords")}
+                  </p>
                   <Image
                     src={arrowRight}
-                    alt={t('arrowRightAlt')}
+                    alt={t("arrowRightAlt")}
                     className={styles.arrowIcon}
                   />
                 </div>
@@ -183,15 +189,15 @@ const ContactsPage: React.FC = () => {
             </div>
             <div
               className={styles.optionRow}
-              onClick={() => openModal('forBuyers')}
+              onClick={() => openModal("forBuyers")}
             >
               <div className={styles.frame85}>
                 <div className={styles.line17}></div>
                 <div className={styles.frame84}>
-                  <p className={styles.optionText}>{t('forBuyersTenants')}</p>
+                  <p className={styles.optionText}>{t("forBuyersTenants")}</p>
                   <Image
                     src={arrowRight}
-                    alt={t('arrowRightAlt')}
+                    alt={t("arrowRightAlt")}
                     className={styles.arrowIcon}
                   />
                 </div>
